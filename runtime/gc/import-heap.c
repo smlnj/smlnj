f2ad2c46e0ec34dd4461d509309ebd3759516622
John Reppy <JohnReppy@users.noreply.github.com>
2025-02-10 13:48:02 -0600

    Fix reading of code on macOS/arm64

--- runtime/gc/import-heap.c
+++ runtime/gc/import-heap.c
@@ -364,9 +364,30 @@ PVT void ReadHeap (inbuf_t *bp, ml_heap_hdr_t *hdr, ml_state_t *msp, ml_val_t *e
                 }
 
 	      /* read in the big-objects */
+#ifdef ARCH_ARM64
+                /* on Arm64, we read the code into memory and then copy to the
+                 * memory-mapped executable memory, since trying to read directly
+                 * into the executable memory does not work (at least on macOS).
+                 */
+                {
+                    char buffer[8*ONE_K];
+                    off_t nb = totSizeB;
+                    char *dst = (char *)freeObj->obj;
+                    while (nb > 0) {
+                        off_t readSz = (nb > sizeof(buffer)) ? sizeof(buffer) : nb;
+                        if (HeapIO_ReadBlock (bp, buffer, readSz) == FAILURE) {
+                            Die("failure to read big-object data\n");
+                        }
+                        memcpy (dst, buffer, readSz);
+                        dst += readSz;
+                        nb -= readSz;
+                    }
+                }
+#else
 		if (HeapIO_ReadBlock (bp, (void *)(freeObj->obj), totSizeB) == FAILURE) {
                     Die("failure to read big-object data\n");
                 }
+#endif
 		if (j == CODE_INDX) {
 		    FlushICache ((void *)(freeObj->obj), totSizeB);
 		}
