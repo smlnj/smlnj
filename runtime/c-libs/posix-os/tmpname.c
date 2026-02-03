6e83c61d22cfd0a40787d72991afaa945318249b
John Reppy <JohnReppy@users.noreply.github.com>
2026-02-03 08:06:53 -0600

    merge legacy pull request #374

--- runtime/c-libs/posix-os/tmpname.c
+++ runtime/c-libs/posix-os/tmpname.c
@@ -6,14 +6,15 @@
 
 #include "ml-unixdep.h"
 #include <stdio.h>
+#include <limits.h>
 #include "ml-base.h"
 #include "ml-c.h"
 #include "ml-values.h"
 #include "ml-objects.h"
 #include "cfun-proto-list.h"
 
-#if defined(HAS_MKSTEMP) && defined(P_tmpdir)
-#  define TEMPLATE	P_tmpdir "/SMLNJ-XXXXXX"
+#ifndef PATH_MAX
+#define PATH_MAX 256
 #endif
 
 /* _ml_OS_tmpname:
@@ -23,18 +24,28 @@ ml_val_t _ml_OS_tmpname (ml_state_t *msp, ml_val_t arg)
 #if defined(HAS_MKSTEMP) && defined(P_tmpdir)
 
   /* mkstemp was added to the IEEE Std 1003.1 in 2004, so most systems should support it */
-    char	template[sizeof(TEMPLATE)];
-    int		sts;
+    char template[PATH_MAX];
+    int sts;
+    const char *tmpdir = getenv ("TMPDIR");
 
-    strcpy (template, TEMPLATE);
-    sts = mkstemp (template);
-
-    if (sts < 0) {
-	return RAISE_SYSERR(msp, sts);
+    if (tmpdir != NIL(char *)) {
+        int r = snprintf (template, PATH_MAX, "%s/SMLNJ-XXXXXX", tmpdir);
+        if (r < sizeof(template)) {
+            sts = mkstemp (template);
+            if (sts >= 0) {
+                close (sts);
+                return ML_CString(msp, template);
+            }
+        }
     }
-    else {
-	close (sts);  /* close the file descriptor */
-	return ML_CString (msp, template);
+
+    strcpy (template, P_tmpdir "/SMLNJ-XXXXXX");
+    sts = mkstemp (template);
+    if (sts < 0) {
+        return RAISE_SYSERR(msp, sts);
+    } else {
+        close (sts);
+        return ML_CString (msp, template);
     }
 
 #else /* for old systems */
